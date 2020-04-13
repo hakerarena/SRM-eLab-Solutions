1fadb8ac355ae82189fee446b863453f967a49a8
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2020-04-14 04:38:58 +0530

    Create Noise Levels.py

---
+++ Python/Noise Levels.py
@@ -0,0 +1,10 @@
+noise=int(input())
+if noise==130:
+  print("Jackhammer")
+elif noise==106:
+  print("Gas lawnmower")
+elif noise==70:
+  print("Alarm clock")
+elif noise==40:
+  print("Quiet room")
+#Code by hakerarenatutorials.wordpress.com
