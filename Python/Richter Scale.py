16b57f8183c67ddc6594f50790acef71a78a8fe2
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2020-04-14 04:39:22 +0530

    Create Richter Scale.py

---
+++ Python/Richter Scale.py
@@ -0,0 +1,20 @@
+mag=float(input())
+if mag>=10.0:
+  print("Meteoric")
+elif mag>=8.0:
+  print("Great")
+elif mag>=7.0:
+  print("Major")
+elif mag>=6.0:
+  print("Strong")
+elif mag>=5.0:
+  print("Moderate")
+elif mag>=4.0:
+  print("Light")
+elif mag>=4.0:
+  print("Minor")
+elif mag>=3.0:
+  print("Very minor")
+else:
+  print("Micro")
+#Code by hakerarenatutorials.wordpress.com
