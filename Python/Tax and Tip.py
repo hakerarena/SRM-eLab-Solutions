07a155f0c90570c541d94aaa603c8ddd251433c6
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2020-04-14 04:40:28 +0530

    Create Tax and Tip.py

---
+++ Python/Tax and Tip.py
@@ -0,0 +1,5 @@
+cost=int(input())
+tax=round(0.05*cost,2)
+tip=round(0.18*cost,2)
+print("The tax is",tax,"and the tip is",tip,"making the total",(cost+tax+tip))
+#Code by hakerarenatutorials.wordpress.com
