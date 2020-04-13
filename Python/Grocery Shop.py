47271ce51768697e5ccfe32d4b8652dd23468b58
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2020-04-14 04:38:27 +0530

    Create Grocery Shop.py

---
+++ Python/Grocery Shop.py
@@ -0,0 +1,9 @@
+item=input()
+cost=float(input())
+quant=int(input())
+print("Product Details")
+print(item)
+print(cost)
+print(quant)
+print("Bill:",round((cost*quant),2))
+#Code by hakerarenatutorials.wordpress.com
